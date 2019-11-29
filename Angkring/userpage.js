dae01b99a27b21c552a20f41cff3b16df6170a80
rainrr <fwooszh@gmail.com>
2019-11-30 06:39:01 +0700

    ALMOST FINISHED

--- Angkring/userpage.js
+++ Angkring/userpage.js
@@ -1,9 +1,9 @@
-$(".hover").mouseleave(
-    function () {
-      $(this).removeClass(".hover");
-    }
-  );
+  /* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "25rem";
+}
 
-  $(window).scroll(function(){
-    $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
-  });
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
