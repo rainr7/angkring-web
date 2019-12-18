640aef84d2a58764f1abf715efee8b472c717332
rainrr <fwooszh@gmail.com>
2019-12-19 02:48:51 +0700

    Final, added admin page, dashboard and checkout page. Some minor changes

--- Angkring/userpage.js
+++ Angkring/userpage.js
@@ -21,3 +21,120 @@ function animateCSS(element, animationName, callback) {
 
   node.addEventListener('animationend', handleAnimationEnd)
 }
+
+$(document).ready(function(){
+
+  var quantitiy=0;
+     $('.quantity-right-plus2').click(function(e){
+          
+          // Stop acting like a button
+          e.preventDefault();
+          // Get the field name
+          var quantity = parseInt($('#quantity2').val());
+          
+          // If is not undefined
+              
+              $('#quantity2').val(quantity + 1);
+  
+            
+              // Increment
+          
+      });
+  
+       $('.quantity-left-minus2').click(function(e){
+          // Stop acting like a button
+          e.preventDefault();
+          // Get the field name
+          var quantity = parseInt($('#quantity2').val());
+          
+          // If is not undefined
+        
+              // Increment
+              if(quantity>0){
+              $('#quantity2').val(quantity - 1);
+              }
+      });
+
+      $('.quantity-right-plus').click(function(e){
+          
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#quantity').val());
+        
+        // If is not undefined
+            
+            $('#quantity').val(quantity + 1);
+
+          
+            // Increment
+        
+    });
+
+     $('.quantity-left-minus').click(function(e){
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#quantity').val());
+        
+        // If is not undefined
+      
+            // Increment
+            if(quantity>0){
+            $('#quantity').val(quantity - 1);
+            }
+    });
+
+    $('.quantity-right-plus3').click(function(e){
+          
+      // Stop acting like a button
+      e.preventDefault();
+      // Get the field name
+      var quantity = parseInt($('#quantity3').val());
+      
+      // If is not undefined
+          
+          $('#quantity3').val(quantity + 1);
+
+        
+          // Increment
+      
+  });
+
+   $('.quantity-left-minus3').click(function(e){
+      // Stop acting like a button
+      e.preventDefault();
+      // Get the field name
+      var quantity = parseInt($('#quantity3').val());
+      
+      // If is not undefined
+    
+          // Increment
+          if(quantity>0){
+          $('#quantity3').val(quantity - 1);
+          }
+  });
+      
+  });
+
+  $(function(){
+
+    $('body').on('click', '#chckbtn', function(e)
+    {
+        e.preventDefault();
+
+        var link = $(this).attr('href');
+        
+        $('body').append(
+            '<div id="overlay">' +
+            '<img id="loading" src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/35771931234507.564a1d2403b3a.gif">' +
+            '</div>'
+        );
+        
+        setTimeout(function(){
+          $('#overlay').remove();
+         
+        }, 2000); //2 seconds
+    });
+
+})
